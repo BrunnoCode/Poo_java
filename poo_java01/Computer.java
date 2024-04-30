b9d7ea6c89cea0bd7751d51a88e653d94121f7a2
bbotelho <brunnowr.bw@gmail.com>
2024-04-30 21:17:57 +0200

    poo 01 class

--- poo_java01/Computer.java
+++ poo_java01/Computer.java
@@ -1,5 +1,30 @@
-public class Ordenador{
+public class Computer{
   private String name;
   private String brand;
-  
+  private int ram;
+  private int mem; 
+  private float price;
+
+  public Computer(String name, String brand, int ram, int mem, float price){
+    this.name = name;
+    this.brand = brand;
+    this.ram = ram;
+    this.mem = mem;
+    this.price = price;
+    System.out.println("Computer completed!");
+  }
+  public Computer(){};
+
+  public void setName(String name){
+    this.name = name;
+  }
+  public void getName(){
+    System.out.println(this.name);
+  }
+  public void setPrice(float price){
+    this.price = price;
+  }
+  public float getPrice(){
+    return this.price;
+  }
 }
