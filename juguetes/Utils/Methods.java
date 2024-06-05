ed9dcf0943a9814cc1c533f974636a2b21b586e0
bbotelho <brunnowr.bw@gmail.com>
2024-06-05 18:16:42 +0200

    automatic save home

--- juguetes/Utils/Methods.java
+++ juguetes/Utils/Methods.java
@@ -1,7 +1,20 @@
 package Utils;
 
 public class Methods{
+
+  //UTILS METHODS
   public static float sizeCalculate(float sizeX, float sizeY, float sizeZ){
       return (sizeX + sizeY + sizeZ);
   }
+
+  public static void selectMessage(){
+    System.out.println("1-Crear lista de Juguetes.\n2-Elegir Juguete a ser enviado.\n3-Listar todos los juguetes añadidos.\n4-Editar o eliminar juguetes\n5-salir");
+  }
+
+  public static void cleanScreen(){
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
+  //USR METHODS
+  
 }
