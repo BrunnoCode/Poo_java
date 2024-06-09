f2f5455a34790310aad8335cc6672bae614b388f
bbotelho <brunnowr.bw@gmail.com>
2024-06-09 11:52:26 +0200

    automatic save home

--- crud/Comida.java
+++ crud/Comida.java
@@ -3,4 +3,54 @@ public class Comida{
   private String origin;
   private float weight;
   private double price;
+
+  public Comida(){}
+
+  public Comida(String foodName, String origin, float weight, double price){
+    this.foodName = foodName;
+    this.origin = origin;
+    this.weight = weight;
+    this.price = price;
+  }
+
+  public String getFoodName() {
+    return foodName;
+  }
+
+  public void setFoodName(String foodName) {
+    this.foodName = foodName;
+  }
+
+  public String getOrigin() {
+    return origin;
+  }
+
+  public void setOrigin(String origin) {
+    this.origin = origin;
+  }
+
+  public float getWeight() {
+    return weight;
+  }
+
+  public void setWeight(float weight) {
+    this.weight = weight;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public String toString(){
+    String peso = getWeight() >= 1.00 ? "kg" : "g";
+    return
+          "Alimento: "+getFoodName()+"\n"+
+          "Origen: "+getOrigin()+"\n"+
+          "Peso: "+getWeight()+peso+"\n"+
+          "Precio: "+getPrice()+"€\n";
+  }
 }
--- crud/SuperMercadoApp.java
+++ crud/SuperMercadoApp.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SuperMercadoApp{
   public static void main(String[] args) {
     ArrayList<Comida> carroCompras = new ArrayList<>();
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Este es el Carrito de la");
   }
 
 }
