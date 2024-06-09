9e4087e30f11da724c2b0d11c6f064fd4cc784aa
bbotelho <brunnowr.bw@gmail.com>
2024-06-09 12:16:41 +0200

    automatic save home

--- crud/SuperMercadoApp.java
+++ crud/SuperMercadoApp.java
@@ -5,7 +5,14 @@ public class SuperMercadoApp{
   public static void main(String[] args) {
     ArrayList<Comida> carroCompras = new ArrayList<>();
     Scanner scan = new Scanner(System.in);
-    System.out.println("Este es el Carrito de la");
-  }
+    System.out.println("*-".repeat(5)+"\n Carrito de Compras \n"+"*-".repeat(5));
+    String usrScan;
+    boolean exit = false;
+    while (!exit){
+      
+    }
+   
 
+    scan.close();
+  }
 }
