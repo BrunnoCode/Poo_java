802edc1a89df57f9c0d6ba53b1e3f45c1e6a2b1a
bbotelho42 <bbotelho@student.42barcelona.com>
2024-10-15 11:46:51 +0200

    automatic save home

--- Ejercicio1/src/main/java/com/mycompany/ejercicio1/Supplier.java
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/Supplier.java
@@ -1,7 +1,26 @@
 package com.mycompany.ejercicio1;
 
-
+import java.time.LocalDate;
 
 public class Supplier extends Person {
+  private Category category;
+
+  public Supplier(int idPerson, String name, String email, String tel, boolean isIndividualPerson, LocalDate startDate,
+      LocalDate deactivationDate, boolean active, Category category) {
+    super(idPerson, name, email, tel, isIndividualPerson, startDate, deactivationDate, active);
+    this.category = category;
+  }
+
+  public Supplier(Category category) {
+    this.category = category;
+  }
+
+  public Category getCategory() {
+    return category;
+  }
+
+  public void setCategory(Category category) {
+    this.category = category;
+  }
   
 }
