ac3cf650cce99cfbce040c45a366f7b84dfcfa93
bbotelho42 <bbotelho@student.42barcelona.com>
2024-10-15 12:50:49 +0200

    automatic save home

--- Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -12,17 +12,7 @@ import java.time.LocalDate;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Ejercicio 1");
-        Person persona1;
-        persona1 = new Person(
-            4588,
-            "Jose",
-            "josemanoel@gmail.com",
-            "+34 654 984 655",
-            false,
-            LocalDate.now(),
-            LocalDate.now(), true
-        );
-        System.out.println(persona1.toString());
+       System.out.println("Gestor de Supermercado");
+        
     }
 }
--- Ejercicio1/src/main/java/com/mycompany/ejercicio1/Role.java
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/Role.java
@@ -1,5 +1,19 @@
 package com.mycompany.ejercicio1;
 
 public class Role {
-  
+  private int idRole;
+  private String role;
+
+  public int getIdRole() {
+    return idRole;
+  }
+  public void setIdRole(int idRole) {
+    this.idRole = idRole;
+  }
+  public String getRole() {
+    return role;
+  }
+  public void setRole(String role) {
+    this.role = role;
+  }
 }
