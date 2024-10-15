02c56ed7122f02f2d6a06a4bd15cfc75ac4c2d2a
bbotelho42 <bbotelho@student.42barcelona.com>
2024-10-15 10:31:36 +0200

    automatic save home

--- Ejercicio1/src/main/java/com/mycompany/ejercicio1/Stock.java
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/Stock.java
@@ -7,21 +7,24 @@ public class Stock {
   private int idStock;
   private List<Category> category = new ArrayList<>();
 
-  
   public int getIdStock() {
     return idStock;
   }
- public Stock(int idStock, List<Category> category) {
-  this.idStock = idStock;
-  this.category = category;
-}
- public void setIdStock(int idStock) {
+
+  public Stock(int idStock, List<Category> category) {
+    this.idStock = idStock;
+    this.category = category;
+  }
+
+  public void setIdStock(int idStock) {
     this.idStock = idStock;
   }
+
   public List<Category> getCategory() {
     return category;
   }
+
   public void setCategory(List<Category> category) {
     this.category = category;
   }
-  }
+}
--- Ejercicio1/src/main/java/com/mycompany/ejercicio1/User.java
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/User.java
@@ -1,10 +1,55 @@
 package com.mycompany.ejercicio1;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author bbotelho42
  */
 public class User extends Person{
+  private String userName;
+  private String password;
+  private String role;
+  
+  public User(int idPerson, String name, String email, String tel, boolean isIndividualPerson, LocalDate startDate,
+      LocalDate deactivationDate, boolean active, String userName, String password, String role) {
+    super(idPerson, name, email, tel, isIndividualPerson, startDate, deactivationDate, active);
+    this.userName = userName;
+    this.password = password;
+    this.role = role;
+  }
+  public User(String userName, String password, String role) {
+    this.userName = userName;
+    this.password = password;
+    this.role = role;
+  }
+  public String getUserName() {
+    return userName;
+  }
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  public String getRole() {
+    return role;
+  }
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  @Override
+
+  public String toString(){
+    return 
+          "\nInformacion usuario:\n---------------------"+
+          "UserName: "+getUserName()+"\n"+
+          "Rol: "+getRole()+"\n";
+  }
   
   
 }
